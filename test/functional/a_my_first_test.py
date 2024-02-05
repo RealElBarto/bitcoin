bba16fb1a40a9f228e4093658138584bf6cc806b
henmeh <143960925+henmeh@users.noreply.github.com>
2024-02-05 16:44:04 +0100

    a_my_first_test.py::first try to understand the testframework

---
+++ test/functional/a_my_first_test.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import (
+    assert_equal,
+)
+
+class ExampleTest(BitcoinTestFramework):
+    def add_options(self, parser):
+        self.add_wallet_options(parser)
+
+    def set_test_params(self):
+        self.setup_clean_chain = True
+        self.num_nodes = 3
+        self.extra_args = [[], ["-logips"], []]
+    
+    def skip_test_if_missing_module(self):
+        self.skip_if_no_wallet()
+
+    #def custom_method(self):
+    #    """Do some custom behaviour for this test
+
+    #    Define it in a method here because you're going to use it repeatedly.
+    #    If you think it's useful in general, consider moving it to the base
+    #    BitcoinTestFramework class so other tests can use it."""
+
+    #    self.log.info("Running custom_method")
+
+    def run_test(self):
+        """Main test logic"""
+        
+        assert_equal (self.nodes[0].get_deterministic_priv_key().address, 'mjTkW3DjgyZck4KbiRusZsqTgaYTxdSz6z')
+        assert_equal (self.nodes[0].get_deterministic_priv_key().key, 'cVpF924EspNh8KjYsfhgY96mmxvT6DgdWiTYMtMjuM74hJaU5psW')
+        
+if __name__ == '__main__':
+    ExampleTest().main()
